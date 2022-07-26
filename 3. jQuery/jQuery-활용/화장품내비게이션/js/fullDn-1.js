a307c1e61278469cc949d7ae3e18daec4ce908d7
changonpark <changonpark@naver.com>
2022-07-26 17:37:05 +0900

    Test25-1

--- 3. jQuery/jQuery-활용/화장품내비게이션/js/fullDn-1.js
+++ 3. jQuery/jQuery-활용/화장품내비게이션/js/fullDn-1.js
@@ -1,39 +1,38 @@
 $(function(){
 
-  var $gnb      = $('#gnb > li');                 //1차메뉴
-  var $sub      = $('.sub');                      //2차메뉴
-  var headerMin = $('#headerWrap').height();      //1차메뉴 높이(30px)
-  var headerMax = headerMin + $sub.innerHeight(); //1차+2차메뉴 높이
-  var speed     = 200;                            //시간(ms)
-  var flag      = false;                          //메뉴상태
+  var $gnb      = $('#gnb > li');
+  var $sub      = $('.sub');
+  var headerMin = $('#headerWrap').height();
+  var headerMax = headerMin + $sub.innerHeight();
+  var speed     = 200;
+  var flag      = false;
 
-// 2차 메뉴 숨기기
+
 $sub.hide();
 
-// 1차메뉴에 마우스 커서가 들어가면 풀다운
+
 $gnb.mouseenter(function(){
 
   if( !flag ){
     $('#headerWrap').stop().animate({ 
       height: headerMax
     }, speed, function(){
-      //sub.show();
-      $sub.stop().fadeIn(speed); //0.2초동안 2차메뉴를 나오게 해라.
+      $sub.stop().fadeIn(speed);
     });
     flag = true;
   }
 });
 
-// 1차메뉴에서 마우스 커서가 나가면 풀업
+
 $gnb.mouseleave(function(){
-  // 1차메뉴에서 .on 제거
+
   $(this).find('a').removeClass('on');
 
-  // 2차메뉴에서 .on 제거
+
   $(this).find('ul').removeClass('on');
 });
 
-// 헤더 영역에서 마우스 커서가 나가면 2차메뉴 닫기
+
 $('#head').mouseleave(function(){
   $sub.stop().fadeOut(speed, function(){
     $('#headerWrap').stop().animate( {height: headerMin}, speed);
